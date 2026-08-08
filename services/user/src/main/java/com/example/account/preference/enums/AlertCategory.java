21f864e316653253afd8e4cfd351b22ac8838660
Charles Jarju <charlesjarju@gmail.com>
2026-08-08 09:12:51 +0200

    feat: add AlertCategory and DeliveryChannel enums

---
+++ services/user/src/main/java/com/example/account/preference/enums/AlertCategory.java
@@ -0,0 +1,12 @@
+package com.example.account.preference.enums;
+
+public enum AlertCategory {
+    MARKETING,
+    ORDER,
+    PAYMENT,
+    BILLING,
+    SHIPPING,
+    SECURITY,
+    ACCOUNT,
+    SYSTEM
+}
