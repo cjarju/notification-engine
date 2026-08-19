344eb03d3843121605ca12fc1d2d5e0d43d6b4cb
Charles Jarju <charlesjarju@gmail.com>
2026-08-19 16:17:04 +0200

    feat: add Gateway constants
    - API paths
    - Header constants
    - Service info

---
+++ services/gateway/src/main/java/com/example/gateway/constants/HeaderConstants.java
@@ -0,0 +1,7 @@
+package com.example.gateway.constants;
+
+public final class HeaderConstants {
+    private HeaderConstants() {}
+
+    public static final String CORRELATION_ID = "X-Correlation-ID";
+}
---
+++ services/gateway/src/main/java/com/example/gateway/constants/ServiceInfo.java
@@ -0,0 +1,10 @@
+package com.example.gateway.constants;
+
+public final class ServiceInfo {
+
+    private ServiceInfo() {}
+
+    public static final String NAME = "gateway-service";
+    public static final String DESCRIPTION = "API Gateway";
+    public static final String VERSION = "v1";
+}
